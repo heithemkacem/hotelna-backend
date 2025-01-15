7783fbf4c6e9ad58b14bf37adb5a613ba562ef39
Heithem <heithem.kacem@trimakus.com>
2025-01-15 20:07:44 +0100

    add : expo push token logic

--- hotelna-service/src/services/RabbitMQService.ts
+++ hotelna-service/src/services/RabbitMQService.ts
@@ -1,12 +1,9 @@
 import amqp, { Channel, Connection } from "amqplib";
 import config from "../config/config";
-import { Profile } from "../database";
-import { ApiError } from "../utils";
 
 class RabbitMQService {
-  private requestQueue = "USER_DETAILS_REQUEST";
-  private responseQueue = "USER_DETAILS_RESPONSE";
   private emailQueue = "EMAIL_NOTIFICATION_QUEUE";
+  private sendNotificationQueue = "SEND_NOTIFICATION_QUEUE";
   private connection!: Connection;
   private channel!: Channel;
 
@@ -18,36 +15,13 @@ class RabbitMQService {
     try {
       this.connection = await amqp.connect(config.msgBrokerURL!);
       this.channel = await this.connection.createChannel();
-
-      await this.channel.assertQueue(this.requestQueue);
-      await this.channel.assertQueue(this.responseQueue);
       await this.channel.assertQueue(this.emailQueue);
+      await this.channel.assertQueue(this.sendNotificationQueue);
       process.on("SIGINT", () => this.shutdown());
       process.on("SIGTERM", () => this.shutdown());
-      this.listenForRequests();
     } catch (error) {
       console.error("Failed to initialize RabbitMQ connection:", error);
     }
-  }
-  private async listenForRequests() {
-    this.channel.consume(this.requestQueue, async (msg) => {
-      if (msg && msg.content) {
-        const { userId } = JSON.parse(msg.content.toString());
-        console.log(msg);
-        console.log("rabbitMQ user id in the auth", userId);
-        const userDetails = await getUserDetails(userId);
-
-        // Send the user details response
-        this.channel.sendToQueue(
-          this.responseQueue,
-          Buffer.from(JSON.stringify(userDetails)),
-          { correlationId: msg.properties.correlationId }
-        );
-
-        // Acknowledge the processed message
-        this.channel.ack(msg);
-      }
-    });
   }
   async sendEmailNotification(to: string, subject: string, body: string) {
     const message = { to, subject, body };
@@ -56,6 +30,14 @@ class RabbitMQService {
       Buffer.from(JSON.stringify(message))
     );
     console.log("Email notification request sent");
+  }
+  async sendNotification(to: string, title: string, body: string) {
+    const message = { to, title, body };
+    this.channel.sendToQueue(
+      this.sendNotificationQueue,
+      Buffer.from(JSON.stringify(message))
+    );
+    console.log("Notification request sent");
   }
 
   private async shutdown() {
@@ -71,12 +53,4 @@ class RabbitMQService {
   }
 }
 
-const getUserDetails = async (userId: string) => {
-  const userDetails = await Profile.findById(userId).select("-password");
-  if (!userDetails) {
-    throw new ApiError(404, "User not found");
-  }
-
-  return userDetails;
-};
 export const rabbitMQService = new RabbitMQService();
