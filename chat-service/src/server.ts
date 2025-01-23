6b661baafa8ed1e6e71421c3c3f25d4057417376
Heithem <heithem.kacem@trimakus.com>
2025-01-24 00:32:36 +0100

    rm : chat

--- chat-service/src/server.ts
+++ chat-service/src/server.ts
@@ -10,21 +10,40 @@ app.use(morgan("dev"));
 server = app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
 });
+const userSocketMap = new Map();
 const io = new SocketIOServer(server);
 io.on("connection", (socket: Socket) => {
-  console.log("Client connected");
+  console.log("Client connected", socket.id);
+
+  socket.on("register", (userId) => {
+    userSocketMap.set(userId, socket.id);
+    console.log(`User ${userId} registered with socket ID ${socket.id}`);
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected", socket.id);
+    // Optionally, remove the user from the map if needed
+    for (const [userId, sockId] of userSocketMap.entries()) {
+      if (sockId === socket.id) {
+        userSocketMap.delete(userId);
+        break;
+      }
+    }
   });
 
   socket.on("sendMessage", async (data) => {
-    console.log(data);
     const { senderId, receiverId, message } = data;
     const msg = new Message({ senderId, receiverId, message });
     await msg.save();
-
-    // Assuming receiverId is the socket ID of the receiver
-    io.to(receiverId).emit("receiveMessage", msg);
+    console.log(data);
+    const receiverSocketId = userSocketMap.get(receiverId);
+    console.log("receiverSocketId", receiverSocketId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("receiveMessage", msg);
+    } else {
+      console.log(`User ${receiverId} is not connected`);
+      // Optionally, you could store the message for later delivery
+    }
   });
 });
 const unexpectedErrorHandler = (error: unknown) => {
